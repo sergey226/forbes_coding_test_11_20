2e96834c40ff9da4635d4bad8bcdb0add309389c
Sergey Frolov <sergey226@gmail.com>
2020-11-11 18:48:42 -0500

    Data, fs and cors packages added

--- challenge/index.js
+++ challenge/index.js
@@ -1,7 +1,37 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const { promises: fs } = require("fs")
 
-const createPage = () => {
+app.use(cors())
+
+// Variables and constants:
+const storyFile = 'data/story.txt'
+const dictionaryFile = 'data/dictionary.txt'
+let story = ''
+let dictionary = ''
+
+const readFile = async fileName => {
+  let file = null
+  try {
+    file = await fs.readFile(fileName, "utf-8")
+  } catch (e) {
+      console.log("File reading error", e)
+  }
+  return file
+}
+
+readFile(storyFile).then(result => {
+  story = result
+  console.log(result.slice(0,20))
+})
+
+readFile(dictionaryFile).then(result => {
+  dictionary = result
+  console.log(result.slice(0,20))
+})
+
+function createPage() {
   return '<h1>TESTING 1 2 3!!!!</h1>'
 }
 
